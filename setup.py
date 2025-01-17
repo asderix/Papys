9a40a295de5c0dc5fc603474d47443d7145bcd0c
Ronny Fuchs <info@asderix.com>
2025-01-17 07:44:52 +0100

    New minor version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="papys",
-    version="0.8.4",
+    version="0.8.5",
     author="Ronny Fuchs",
     author_email="info@asderix.com",
     description="Python Rest-API as a DAG",
